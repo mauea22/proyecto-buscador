e4f10e99692f151637fa1aa8a8317b649cabf504
mauea22 <mauroallena@gmail.com>
2022-05-22 23:19:04 -0300

    creando objeto de busqueda

--- js/app.js
+++ js/app.js
@@ -1,11 +1,70 @@
 // Variables
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+//Contenedor para los resultados
 const resultados = document.querySelector('#resultado');
+
+//variables para los option de años
+const max = new Date().getFullYear(); //toma el año usando la fecha actual
+const min = max - 10;
+
+//Genera un objeto con la busqueda
+const datosBusqueda = {
+    marca : '',
+    year : '',
+    minimo : '',
+    maximo : '',
+    puertas: '',
+    transmision : '',
+    color : '',
+}
 
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
+    //Muestra los datos de autos al cargar (array de objetos) 
     mostrarAutos();
+    //LLena las opciones de años
+    llenarSelect();
 })
+
+//eventListener para los input del formulario de busqueda
+
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+})
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+})
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+})
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+})
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+})
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+})
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+})
+
 
 
 //Funciones
@@ -20,15 +79,24 @@ function mostrarAutos() {
             ${marca} -
             ${modelo} -
             ${year} -
-            ${precio} -
-            ${puertas} -
-            ${color} -
-            ${transmision}
+            Precio: ${precio} -
+            ${puertas} Puertas -
+            color: ${color} -
+            transmisión: ${transmision}
         `;
 
         //Insertar en el HTML
         resultado.appendChild(autoHTML);
     });
+}
 
-    
+// Genera los años del select
+
+function llenarSelect() {
+    for ( let i = max; i >= min; i--){
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
+    }
 }
